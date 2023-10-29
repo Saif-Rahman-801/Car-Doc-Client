10f192ce2a8b1f868b4209ecce70dce8ba3e1523
Saif-Rahman-801 <saifurs102@gmail.com>
2023-10-29 12:34:13 +0600

    services shown

---
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ServiceCard = ({ service }) => {
+  const { img, title, price } = service;
+  return (
+    <div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src={img} alt="Shoes" className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{title} </h2>
+          <p>{price} </p>
+          <div className="card-actions">
+            <button className="btn rounded-md bg-red-500">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
+
+const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
+  return (
+    <div className="my-5">
+      <div className="text-center">
+        <h3 className="text-3xl text-orange-500 font-semibold">Services</h3>
+        <h2 className="text-5xl font-bold my-3">Our service area</h2>
+        <p className="font-medium">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore earum
+          autem placeat. Perspiciatis maiores
+          <br /> fuga incidunt voluptatibus nemo vero numquam.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 ">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
